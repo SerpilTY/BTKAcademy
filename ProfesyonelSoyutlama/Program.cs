5bd5eddda2b269bba84c483dd6f46b7cb6d4bcd9
Serpil81 <44366185+Serpil81@users.noreply.github.com>
2023-02-07 21:46:08 +0300

    Initial Commit

---
+++ ProfesyonelSoyutlama/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace ProfessionalAbstraction;
+class Program
+{
+    static void Main(string[] args)
+    {
+        CustomerManager cm=new CustomerManager();
+        cm.Logger=new DatabaseLogger();
+        cm.Add();
+    }
+}
+
+class CustomerManager{
+
+    public ILogger Logger { get; set; }
+    public void Add(){
+        Logger.Log();
+       System.Console.WriteLine("Customer Added!");
+    }
+}
+
+class DatabaseLogger:ILogger{
+
+    public void Log(){
+        System.Console.WriteLine("Logged To DB!");
+    }
+}
+class FileLogger : ILogger
+{
+    public void Log()
+    {
+        System.Console.WriteLine("Logged To File!");
+    }
+}
+interface ILogger{
+    void Log();
+}
+
+
+
+
